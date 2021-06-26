dd9ab6d316587c6aa89b52a0c65d8d28056b71e6
VigneshReddyJulakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2021-06-26 12:33:47 +0530

    1

---
+++ auto_whatsapp.py
@@ -0,0 +1,16 @@
+import selenium
+from selenium import webdriver
+import keyboard
+import time
+
+driver=webdriver.Chrome(executable_path='C:\\Users\\DELL\\Documents\\webdrivers\\chromedriver.exe')
+driver.get("https://web.whatsapp.com/")
+driver.maximize_window()
+driver.implicitly_wait(200)
+driver.find_element_by_xpath("//*[@id='pane-side']/div[1]/div/div/div[9]/div/div/div[2]/div[1]/div[1]/span").click()
+driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]").click()
+for i in range(100):
+    # keyboard.write(f"{i}\n")
+    keyboard.write("{0}.Subscribe timepass coders for more such stuff\n".format(i))
+    # time.sleep(1)
+    
