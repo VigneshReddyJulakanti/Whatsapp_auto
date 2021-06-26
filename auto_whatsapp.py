5d16d5cc84538c9469e5f4d9350e752cc2c25ada
VigneshReddyJulakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2021-06-26 12:39:56 +0530

    Update auto_whatsapp.py

--- auto_whatsapp.py
+++ auto_whatsapp.py
@@ -1,13 +1,20 @@
-import selenium
+import selenium   #pip install selenium
 from selenium import webdriver
 import keyboard
 import time
 
-driver=webdriver.Chrome(executable_path='C:\\Users\\DELL\\Documents\\webdrivers\\chromedriver.exe')
+######
+#NOTE
+#Dont run this before changing xpath , orelse it may send messages to other groups
+
+
+######
+
+driver=webdriver.Chrome(executable_path='C:\\Users\\DELL\\Documents\\webdrivers\\chromedriver.exe')  #Replace this path with the path where you have extracted ur driver
 driver.get("https://web.whatsapp.com/")
 driver.maximize_window()
 driver.implicitly_wait(200)
-driver.find_element_by_xpath("//*[@id='pane-side']/div[1]/div/div/div[9]/div/div/div[2]/div[1]/div[1]/span").click()
+driver.find_element_by_xpath("//*[@id='pane-side']/div[1]/div/div/div[9]/div/div/div[2]/div[1]/div[1]/span").click()   #change the Xpath
 driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]").click()
 for i in range(100):
     # keyboard.write(f"{i}\n")
